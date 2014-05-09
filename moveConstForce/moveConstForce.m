8b5701b416783ec13812817077f26f117a0808bf
seehma <matthias@seehauser.at>
2014-05-09 17:39:14 +0200

    modified force control example
    modified and tested force control example;

--- moveConstForce/moveConstForce.m
+++ moveConstForce/moveConstForce.m
@@ -2,15 +2,17 @@
 % initialize variables
 % ---------------------------------------------------------------------------------
 averageSize = 20;
-searchSeconds = 5;
-controlSeconds = 0.830;
-kp = 1;
-ki = 1;
-dt = 0.1;
+searchSeconds = 15;
+controlSeconds = 15;
+kp = 2;
+ki = 6;
+dt = 0.05;
 kd = 0.0;
 smallestControlValue = 0.001;
 t_ipo = 0.012;
 ipolCyclesPerSecond = floor(1/0.012);
+
+
 
 %% --------------------------------------------------------------------------------
 % first find the force without contact
@@ -26,22 +28,32 @@ for i=1:1:averageSize
 end
 % calculate average of every force
 forceAveraged = mean(forceAveraged);
-forceSearch = forceAveraged(1) + 0.1*forceAveraged(1);
-forceToHave = forceSearch + 0.2*forceAveraged(1);
+forceSearch = forceAveraged(1) - 0.1*forceAveraged(1);
+forceToHave = forceSearch - 0.0*forceAveraged(1);
 
 %% --------------------------------------------------------------------------------
 % start movement in X axis and wait till average force +10% is reached
 % ---------------------------------------------------------------------------------
 maxSearchTime = floor((t_ipo * ipolCyclesPerSecond * searchSeconds)/t_ipo); 
-conHandle.modifyRKorrVariable('RKorrX','0,05');
+conHandle.modifyRKorrVariable('RKorrZ','-0,02');
+
+RIstSearch = zeros(maxSearchTime,6);
+RSolSearch = zeros(maxSearchTime,6);
+AIstSearch = zeros(maxSearchTime,6);
+ASolSearch = zeros(maxSearchTime,6);
+MACurSearch = zeros(maxSearchTime,6);
+
+objectFound = 0;
+
 for i=1:1:maxSearchTime
   innerLoop=tic();
   
-  [~,~,~,~,~,forceAct] = conHandle.decodeRobotInfoString( conHandle.getAktRobotInfo() );
+  [RIstSearch(i,:),RSolSearch(i,:),AIstSearch(i,:),ASolSearch(i,:),MACurSearch(i,:),forceAct] = conHandle.decodeRobotInfoString( conHandle.getAktRobotInfo() );
   
   % stop when force is 10% bigger than averaged force
-  if( forceAct(1) > forceSearch )
-    conHandle.modifyRKorrVariable('RKorrX','0,0');
+  if( forceAct(1) < forceSearch )
+    conHandle.modifyRKorrVariable('RKorrZ','0,0');
+    objectFound = 1;
     break;
   end
   
@@ -50,14 +62,24 @@ for i=1:1:maxSearchTime
 end
 
 % stop movement for sure after looping
-conHandle.modifyRKorrVariable('RKorrX','0,0');
+conHandle.modifyRKorrVariable('RKorrZ','0,0');
+test2 = 1;  
 
 %% --------------------------------------------------------------------------------
 % now start movement (Y-direction) in other direction and try to control the force
 % ---------------------------------------------------------------------------------
-conHandle.modifyRKorrVariable('RKorrY','0,05');
+conHandle.modifyRKorrVariable('RKorrX','0,05');
+conHandle.modifyRKorrVariable('RKorrZ','0,0');
+
 maxControlTime = floor((t_ipo * ipolCyclesPerSecond * controlSeconds)/t_ipo);
 systemDeviation = 0;
+
+RIstCtrl = zeros(maxControlTime,6);
+RSolCtrl = zeros(maxControlTime,6);
+AIstCtrl = zeros(maxControlTime,6);
+ASolCtrl = zeros(maxControlTime,6);
+MACurCtrl = zeros(maxControlTime,6);
+forceAct = zeros(maxControlTime,6);
 
 integral = 0;
 derivative = 0;
@@ -66,10 +88,10 @@ prevSystemDeviation = 0;
 for i=1:1:maxControlTime
   innerLoop=tic();
   
-  [~,~,~,~,~,forceAct] = conHandle.decodeRobotInfoString( conHandle.getAktRobotInfo() );
+  [RIstCtrl(i,:),RSolCtrl(i,:),AIstCtrl(i,:),ASolCtrl(i,:),MACurCtrl(i,:),forceAct(i,:)] = conHandle.decodeRobotInfoString( conHandle.getAktRobotInfo() );
   
   % calculate power part of PID
-  systemDeviation = forceToHave - forceAct(1);
+  systemDeviation = forceToHave - forceAct(i,1);
   controlValueKP = kp*systemDeviation;
   % calculate integral part of PID
   integral = integral + systemDeviation*dt;
@@ -85,12 +107,15 @@ for i=1:1:maxControlTime
   if( controlValue > 0.05 )
     controlValue = 0.05;
   end
+  if( controlValue < -0.05)
+    controlValue = -0.05;
+  end
   
   % convert to string
   controlValueAsString = num2str(controlValue,'%5.6f');
   controlValueAsString = strrep(controlValueAsString, '.', ',')
   
-  %conHandle.modifyRKorrVariable('RKorrX',controlValueAsString);
+  conHandle.modifyRKorrVariable('RKorrZ',controlValueAsString);
   
   prevSystemDeviation = systemDeviation;
   
@@ -103,3 +128,4 @@ end
 % stop movement in all directions
 conHandle.modifyRKorrVariable('RKorrX','0,0');
 conHandle.modifyRKorrVariable('RKorrY','0,0');
+conHandle.modifyRKorrVariable('RKorrZ','0,0');
