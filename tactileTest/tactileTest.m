d965f3ac5828f6c61d7de7829655c161ddadceec
seehma <matthias@seehauser.at>
2014-05-14 21:51:01 +0200

    modify comments
    added comments to force observing matlab script

--- tactileTest/tactileTest.m
+++ tactileTest/tactileTest.m
@@ -1,29 +1,30 @@
-
+% maximum loopcount to observe the force
 loopCount = 1300;
-
+% initialize variables
 RIst = zeros(loopCount,6);
 RSol = zeros(loopCount,6);
 AIst = zeros(loopCount,6);
 ASol = zeros(loopCount,6);
 MACur = zeros(loopCount,6);
 FT = zeros(loopCount,7);
-
+% drive with 0.1mm per 12ms in negative z-direction
 zKorr = '-0,1';
+% send command to robot
 conHandle.modifyRKorrVariable('RKorrZ',zKorr);
-
-wholeLoop=tic();
+% start loop
 for i=1:1:loopCount
+  % start time measuring
   innerLoop=tic();
-  
+  % get actual values from robot
   [RIst(i,:),RSol(i,:),AIst(i,:),ASol(i,:), MACur(i,:), FT(i,:)] = conHandle.decodeRobotInfoString( conHandle.getAktRobotInfo() );
-  
+  % observe force value in z-direction
   if( FT(i,1) < 0.0150)
+    % if bigger than limit, stop movement
     conHandle.modifyRKorrVariable('RKorrZ','0,0');
-    %break;
   end
-
+  % one cycle duration is 12 ms
   while( toc(innerLoop) < 0.011 )
   end
 end
-toc(wholeLoop)
+% securely stop movement
 conHandle.modifyRKorrVariable('RKorrZ','0,0');
